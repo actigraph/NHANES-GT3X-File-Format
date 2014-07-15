34e2c3f89e4f858d9901280e478875c33ca14a84
Daniel Judge <dwjref@gmail.com>
2014-07-15 17:45:50 -0500

    Added some equality and tostring for samples class

--- src/GT3X.Parsing.Library/AccelerationSample.cs
+++ src/GT3X.Parsing.Library/AccelerationSample.cs
@@ -1,8 +1,22 @@
+using System;
+
 namespace GT3X.Parsing.Library
 {
-    public class AccelerationSample
+    public class AccelerationSample : IEquatable<AccelerationSample>
     {
-        public double X, Y, Z;
+        public readonly double X;
+        public readonly double Y;
+        public readonly double Z;
+
+        public override string ToString()
+        {
+            return string.Format("Y = {0} | X = {1} | Z = {2}", Y, X, Z);
+        }
+
+        bool IEquatable<AccelerationSample>.Equals(AccelerationSample other)
+        {
+            return Equals(other);
+        }
 
         public AccelerationSample()
         {
@@ -21,6 +35,42 @@ namespace GT3X.Parsing.Library
             X = sample[1];
             Y = sample[0];
             Z = sample[2];
+        }
+
+        public bool Equals(AccelerationSample other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return X.Equals(other.X) && Y.Equals(other.Y) && Z.Equals(other.Z);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((AccelerationSample) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = X.GetHashCode();
+                hashCode = (hashCode*397) ^ Y.GetHashCode();
+                hashCode = (hashCode*397) ^ Z.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(AccelerationSample left, AccelerationSample right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(AccelerationSample left, AccelerationSample right)
+        {
+            return !Equals(left, right);
         }
     }
 }
