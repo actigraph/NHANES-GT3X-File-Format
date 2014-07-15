1e0054157b7a2f081c632a956745dd60497c39a7
Daniel Judge <dwjref@gmail.com>
2014-07-15 17:46:37 -0500

    Added unit tests for parsing activity and lux file
    Using data from the examples in the readmes, we verify that the parsers
    work properly

--- src/GT3X.Parsing.Library.Tests/LibraryTests.cs
+++ src/GT3X.Parsing.Library.Tests/LibraryTests.cs
@@ -1,11 +1,18 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Xunit;
 using Ionic.Zip;
 
 namespace GT3X.Parsing.Library.Tests
 {
-    public class LibraryTests
+    public class LibraryTests : IDisposable
     {
+        private string _filename;
+        const string ACTIVITY_BIN = "activity.bin";
+        const string LUX_BIN = "lux.bin";
+        const string INFO_TXT = "info.txt";
+
         [Fact]
         public void EmptyFileNameIsNotParsable()
         {
@@ -27,127 +34,83 @@ namespace GT3X.Parsing.Library.Tests
         [Fact]
         public void NonZipFileIsNotParsable()
         {
-            string filename = Path.GetTempFileName();
+            _filename = Path.GetTempFileName();
 
-            Assert.Throws<FileIsNotAZipFile>(() => new Gt3XFile(filename));
-
-            //clean up temporary file
-            try { File.Delete(filename); }
-            catch {  }
+            Assert.Throws<FileIsNotAZipFile>(() => new Gt3XFile(_filename));
         }
 
         [Fact]
         public void ZipFileWithNoFilesIsNotParsable()
         {
-            string filename = Path.GetTempFileName();
+            _filename = Path.GetTempFileName();
 
             using (ZipFile zip = new ZipFile())
-                zip.Save(filename);
+                zip.Save(_filename);
 
-            Assert.Throws<MissingRequiredFiles>(() => new Gt3XFile(filename));
-
-            //clean up temporary file
-            try { File.Delete(filename); }
-            catch { }
+            Assert.Throws<MissingRequiredFiles>(() => new Gt3XFile(_filename));
         }
 
         [Fact]
         public void ZipFileWithNoActivityBinIsNotParsable()
         {
-            string filename = Path.GetTempFileName();
+            _filename = Path.GetTempFileName();
 
-            const string LUX_BIN = "lux.bin";
             using (File.Create(LUX_BIN)) { }
-            const string INFO_TXT = "info.txt";
             using (File.Create(INFO_TXT)) { }
 
             using (ZipFile zip = new ZipFile())
             {
                 zip.AddFile(LUX_BIN);
                 zip.AddFile(INFO_TXT);
-                zip.Save(filename);
+                zip.Save(_filename);
             }
 
-            Assert.Throws<MissingRequiredFiles>(() => new Gt3XFile(filename));
-
-            //clean up temporary file
-            try 
-            { 
-                File.Delete(filename);
-                File.Delete(LUX_BIN);
-                File.Delete(INFO_TXT);
-            }
-            catch { }
+            Assert.Throws<MissingRequiredFiles>(() => new Gt3XFile(_filename));
         }
 
         [Fact]
         public void ZipFileWithNoLuxBinIsNotParsable()
         {
-            string filename = Path.GetTempFileName();
+            _filename = Path.GetTempFileName();
 
-            const string ACTIVITY_BIN = "activity.bin";
             using (File.Create(ACTIVITY_BIN)) { }
-            const string INFO_TXT = "info.txt";
             using (File.Create(INFO_TXT)) { }
 
             using (ZipFile zip = new ZipFile())
             {
                 zip.AddFile(ACTIVITY_BIN);
                 zip.AddFile(INFO_TXT);
-                zip.Save(filename);
+                zip.Save(_filename);
             }
 
-            Assert.Throws<MissingRequiredFiles>(() => new Gt3XFile(filename));
-
-            //clean up temporary file
-            try
-            {
-                File.Delete(filename);
-                File.Delete(ACTIVITY_BIN);
-                File.Delete(INFO_TXT);
-            }
-            catch { }
+            Assert.Throws<MissingRequiredFiles>(() => new Gt3XFile(_filename));
         }
 
         [Fact]
         public void ZipFileWithNoInfoTxtIsNotParsable()
         {
-            string filename = Path.GetTempFileName();
+            _filename = Path.GetTempFileName();
 
-            const string ACTIVITY_BIN = "activity.bin";
             using (File.Create(ACTIVITY_BIN)) { }
-            const string LUX_BIN = "lux.bin";
             using (File.Create(LUX_BIN)) { }
 
             using (ZipFile zip = new ZipFile())
             {
                 zip.AddFile(ACTIVITY_BIN);
                 zip.AddFile(LUX_BIN);
-                zip.Save(filename);
+                zip.Save(_filename);
             }
             
-            Assert.Throws<MissingRequiredFiles>(() => new Gt3XFile(filename));
-
-            //clean up temporary file
-            try
-            {
-                File.Delete(filename);
-                File.Delete(ACTIVITY_BIN);
-                File.Delete(LUX_BIN);
-            }
-            catch { }
+            Assert.Throws<MissingRequiredFiles>(() => new Gt3XFile(_filename));
         }
 
         [Fact]
         public void ZipFileWithEmptyInfoTxtIsNotParsable()
         {
-            string filename = Path.GetTempFileName();
+            _filename = Path.GetTempFileName();
 
-            const string ACTIVITY_BIN = "activity.bin";
             using (File.Create(ACTIVITY_BIN)) { }
-            const string LUX_BIN = "lux.bin";
             using (File.Create(LUX_BIN)) { }
-            const string INFO_TXT = "info.txt";
             using (File.Create(INFO_TXT)) { }
 
             using (ZipFile zip = new ZipFile())
@@ -155,20 +118,270 @@ namespace GT3X.Parsing.Library.Tests
                 zip.AddFile(ACTIVITY_BIN);
                 zip.AddFile(LUX_BIN);
                 zip.AddFile(INFO_TXT);
-                zip.Save(filename);
+                zip.Save(_filename);
             }
 
-            Assert.Throws<InvalidInfoFile>(() => new Gt3XFile(filename));
+            Assert.Throws<InvalidInfoFile>(() => new Gt3XFile(_filename));
+        }
 
-            //clean up temporary file
-            try
+        [Fact]
+        public void ZipFileWithMissingSerialNumberInfoTxtIsNotParsable()
+        {
+            _filename = Path.GetTempFileName();
+
+            using (File.Create(ACTIVITY_BIN)) { }
+            using (File.Create(LUX_BIN)) { }
+            CreateInfoTxtFile(true, true, false);
+
+            using (ZipFile zip = new ZipFile())
             {
-                File.Delete(filename);
-                File.Delete(ACTIVITY_BIN);
-                File.Delete(LUX_BIN);
-                File.Delete(INFO_TXT);
+                zip.AddFile(ACTIVITY_BIN);
+                zip.AddFile(LUX_BIN);
+                zip.AddFile(INFO_TXT);
+                zip.Save(_filename);
             }
-            catch { }
+
+            Assert.Throws<InvalidInfoFile>(() => new Gt3XFile(_filename));
+        }
+
+        [Fact]
+        public void ZipFileWithMissingSampleRateInfoTxtIsNotParsable()
+        {
+            _filename = Path.GetTempFileName();
+
+            using (File.Create(ACTIVITY_BIN)) { }
+            using (File.Create(LUX_BIN)) { }
+            CreateInfoTxtFile(false, true, true);
+
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AddFile(ACTIVITY_BIN);
+                zip.AddFile(LUX_BIN);
+                zip.AddFile(INFO_TXT);
+                zip.Save(_filename);
+            }
+
+            Assert.Throws<InvalidInfoFile>(() => new Gt3XFile(_filename));
+        }
+
+        [Fact]
+        public void ZipFileWithMissingStartDateInfoTxtIsNotParsable()
+        {
+            _filename = Path.GetTempFileName();
+
+            using (File.Create(ACTIVITY_BIN)) { }
+            using (File.Create(LUX_BIN)) { }
+            CreateInfoTxtFile(true, false, true);
+
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AddFile(ACTIVITY_BIN);
+                zip.AddFile(LUX_BIN);
+                zip.AddFile(INFO_TXT);
+                zip.Save(_filename);
+            }
+
+            Assert.Throws<InvalidInfoFile>(() => new Gt3XFile(_filename));
+        }
+
+        [Fact]
+        public void ZipFileWithValidInfoTxtIsParsable()
+        {
+            _filename = Path.GetTempFileName();
+            
+            using (File.Create(ACTIVITY_BIN)) { }
+            using (File.Create(LUX_BIN)) { }
+            
+            CreateInfoTxtFile(true, true, true);
+
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AddFile(ACTIVITY_BIN);
+                zip.AddFile(LUX_BIN);
+                zip.AddFile(INFO_TXT);
+                zip.Save(_filename);
+            }
+
+            Assert.DoesNotThrow(() => new Gt3XFile(_filename));
+        }
+
+        [Fact]
+        public void ValidZipFileAllZeroActivityParses()
+        {
+            _filename = Path.GetTempFileName();
+
+            using (var file = File.Create(ACTIVITY_BIN))
+                for (int i = 0; i < 27; i++)
+                    file.WriteByte(0);
+
+            using (File.Create(LUX_BIN)) { }
+
+            CreateInfoTxtFile(true, true, true);
+
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AddFile(ACTIVITY_BIN);
+                zip.AddFile(LUX_BIN);
+                zip.AddFile(INFO_TXT);
+                zip.Save(_filename);
+            }
+
+            var g = new Gt3XFile(_filename);
+
+            AccelerationSample zeros = new AccelerationSample(0, 0, 0);
+            foreach (AccelerationSample sample in g.ActivityEnumerator())
+                Assert.Equal(zeros, sample);
+        }
+
+        
+        [Fact]
+        public void ValidZipFileSampleActivityParses()
+        {
+            _filename = Path.GetTempFileName();
+
+            List<byte> bytes = new List<byte>(14)
+            {
+                0x00,
+                0x60,
+                0x08,
+                0xEB,
+                0xD0,
+                0x07,
+                0x00,
+                0x9E,
+                0xBF,
+                0x00,
+                0x70,
+                0x08,
+                0xEB,
+                0xF0
+            };
+
+            using (var file = File.Create(ACTIVITY_BIN))
+                foreach (var b in bytes)
+                    file.WriteByte(b);
+
+            using (File.Create(LUX_BIN)) { }
+
+            CreateInfoTxtFile(true, true, true);
+
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AddFile(ACTIVITY_BIN);
+                zip.AddFile(LUX_BIN);
+                zip.AddFile(INFO_TXT);
+                zip.Save(_filename);
+            }
+
+            var g = new Gt3XFile(_filename);
+
+            int counter = 0;
+
+            List<AccelerationSample> samples = new List<AccelerationSample>(3)
+            {
+                new AccelerationSample(0.023, 0.018, -0.947),
+                new AccelerationSample(0.026, 0.021, -0.941),
+                new AccelerationSample(0.023, 0.021, -0.941)
+            };
+
+            foreach (AccelerationSample sample in g.ActivityEnumerator())
+                Assert.Equal(samples[counter++], sample);
+        }
+
+        [Fact]
+        public void ValidZipFileAllZeroLuxParses()
+        {
+            _filename = Path.GetTempFileName();
+
+            using (var file = File.Create(LUX_BIN))
+                for (int i = 0; i < 27; i++)
+                    file.WriteByte(0);
+
+            using (File.Create(ACTIVITY_BIN)) { }
+
+            CreateInfoTxtFile(true, true, true);
+
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AddFile(ACTIVITY_BIN);
+                zip.AddFile(LUX_BIN);
+                zip.AddFile(INFO_TXT);
+                zip.Save(_filename);
+            }
+
+            var g = new Gt3XFile(_filename);
+
+            foreach (double lux in g.LuxEnumerator())
+                Assert.Equal(0, lux);
+        }
+
+        [Fact]
+        public void ValidZipFileSampleLuxParses()
+        {
+            _filename = Path.GetTempFileName();
+
+            List<byte> bytes = new List<byte>(10)
+            {
+                0x00,
+                0x00,
+                0x13,
+                0x00,
+                0xFF,
+                0xFF,
+                0x98,
+                0x08,
+                0x4C,
+                0x04
+            };
+
+            using (var file = File.Create(LUX_BIN))
+                foreach (var b in bytes)
+                    file.WriteByte(b);
+
+            using (File.Create(ACTIVITY_BIN)) { }
+
+            CreateInfoTxtFile(true, true, true);
+
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AddFile(ACTIVITY_BIN);
+                zip.AddFile(LUX_BIN);
+                zip.AddFile(INFO_TXT);
+                zip.Save(_filename);
+            }
+
+            var g = new Gt3XFile(_filename);
+
+            List<double> luxSamples = new List<double>(5) {0, 0, 0, 2500, 1375};
+
+            int counter = 0;
+            foreach (double lux in g.LuxEnumerator())
+                Assert.Equal(luxSamples[counter++], lux);
+        }
+
+        private static void CreateInfoTxtFile(bool addSampleRate, bool addStartDate, bool addSerialNumber)
+        {
+            //sample rate, start date and serial number
+            using (var file = new StreamWriter(INFO_TXT))
+            {
+                if (addSerialNumber) file.WriteLine("Serial Number: NEO1C16110020");
+                file.WriteLine("Firmware: 2.2.0");
+                file.WriteLine("Battery Voltage: 4.25");
+                if (addSampleRate) file.WriteLine("Sample Rate: 30");
+                if (addStartDate) file.WriteLine("Start Date: 634556532600000000");
+                file.WriteLine("Stop Date: 0");
+                file.WriteLine("Download Date: 634570285571111563");
+                file.WriteLine("Board Revision: 2");
+                file.WriteLine("Subject Name: john doe");
+            }
+        }
+
+        public void Dispose()
+        {
+            try { File.Delete(_filename); } catch { }
+            try { File.Delete(ACTIVITY_BIN); } catch { }
+            try { File.Delete(LUX_BIN); } catch { }
+            try { File.Delete(INFO_TXT); } catch { }
         }
     }
 }
