2dd48c72b3c0de945708a591ea3d7175d5f446de
Daniel Judge <dwjref@gmail.com>
2014-07-30 20:12:10 -0500

    Updated version information

--- src/GT3X.Parsing.Library/Properties/AssemblyInfo.cs
+++ src/GT3X.Parsing.Library/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GT3X.Parsing.Library")]
+[assembly: AssemblyTitle("GT3X Parsing Library")]
 [assembly: AssemblyDescription("Sample library to parse ActiGraph GT3X files")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ActiGraph")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
